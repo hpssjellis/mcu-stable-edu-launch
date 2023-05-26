e70f584b3cc664f02735ab03ffb7c2b8112cb929
Jeremy Ellis <keyfreemusic@gmail.com>
2023-05-26 11:28:38 -0700

    Update esc06.ino

--- xiao-esp32s3/esc1060/esc06.ino
+++ xiao-esp32s3/esc1060/esc06.ino
@@ -1,38 +1,42 @@
 /*
-#if  defined (XIAO_ESP32S3) ||  defined (XIAO_ESP32C3)  //if esp32s3 or esp32C3
+ * this doesn't seem to be working
+#if  defined(XIAO_ESP32S3) ||  defined(XIAO_ESP32C3)  //if esp32s3 or esp32C3
   #include <ESP32Servo.h>
 #else // all other Arduino boards
 //  #include <Servo.h>
 #endif
 */
+
+
 #include <ESP32Servo.h> // esp32 
 //#include <Servo.h>   // most arduinos
-Servo myservo;
+
+Servo myServoPin01;
 
 void setup() {
   Serial.begin(115200);
-  myservo.attach(A1, 1000, 2000); 
+  myServoPin01.attach(A1, 1000, 2000); 
   delay(5000);
   Serial.println("Time to control an ESC Motor driver"); 
 }
 
 void loop() {
 
-  myservo.writeMicroseconds(0);    // not realy needed
+  myServoPin01.writeMicroseconds(0);    // not realy needed
   delay(500);  
-  myservo.writeMicroseconds(1500);  // middle not really needed
+  myServoPin01.writeMicroseconds(1500);  // middle not really needed
   delay(1000);
 
   
-  myservo.writeMicroseconds(1600);  // slow forward
+  myServoPin01.writeMicroseconds(1600);  // slow forward
   Serial.println("Slow forward");
   delay(1000);
 
-  myservo.writeMicroseconds(0);
+  myServoPin01.writeMicroseconds(0);
   delay(500);
-  myservo.writeMicroseconds(1500);  // middle
+  myServoPin01.writeMicroseconds(1500);  // middle
   delay(1000);
-  myservo.writeMicroseconds(1400);  // slow reverse
+  myServoPin01.writeMicroseconds(1400);  // slow reverse
   
   Serial.println("Slow backward");
   delay(1000);
