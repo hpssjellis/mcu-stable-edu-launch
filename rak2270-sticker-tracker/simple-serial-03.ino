29c09583a4e179b60a43646a67905d5474cef69e
Jeremy Ellis <keyfreemusic@gmail.com>
2023-12-05 16:17:52 -0800

    Create simple-serial-03.ino

---
+++ rak2270-sticker-tracker/simple-serial-03.ino
@@ -0,0 +1,23 @@
+void setup() {
+  //Serial.begin(115200, RAK_CUSTOM_MODE);
+  Serial.begin(115200, RAK_AT_MODE);
+  Serial.setTimeout(200);  // To enter AT command mode type "AT" the mode lasts 3 seconds
+}
+
+void loop() {
+  String myString = ""; 
+
+   while(Serial.available() > 0) {
+    char myChar = Serial.read();
+    myString += myChar; // Append the character to the string
+    Serial.print(myChar);
+   }
+   
+   myString.trim();  // get rid of last character
+   if (myString == "Cool"){
+      Serial.println("You entered :"+ myString);
+      Serial.println("Serial.getTimeout(): "+Serial.getTimeout());  
+    
+   }
+
+}
