a2b2d1e25098e63f449f610e0993473b18807795
Jeremy Ellis <keyfreemusic@gmail.com>
2024-06-19 14:15:27 -0700

    Create roboArm-samd.ino

---
+++ xiao-esp32s3/roboArm-samd.ino
@@ -0,0 +1,65 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6;
+
+// Define the pins for each servo
+int servoPin1 = 1;
+int servoPin2 = 2;
+int servoPin3 = 3;
+int servoPin4 = 4;
+int servoPin5 = 5;
+int servoPin6 = 6;
+
+void setup() {
+  // Attach each servo to its respective pin
+
+  pinMode(D0, INPUT_PULLDOWN);
+
+  servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
+  servo3.attach(servoPin3);
+  servo4.attach(servoPin4);
+  servo5.attach(servoPin5);
+  servo6.attach(servoPin6);
+
+
+
+    
+
+  
+}
+
+void loop() {
+  // Initialize servos to starting positions
+  servo1.write(90);
+  servo2.write(0);
+  servo3.write(90);
+  servo4.write(90);
+  servo5.write(0);
+  servo6.write(100);
+  delay(20);
+
+  if (digitalRead(D0) == HIGH){
+
+ // delay(1000); // Delay to allow servos to reach initial positions
+  servo1.write(45);
+    delay(1000); // Delay to allow servos to reach initial positions
+  servo2.write(170);
+    delay(1000); // Delay to allow servos to reach initial positions
+  servo4.write(160);
+    delay(1000); // Delay to allow servos to reach initial positions
+  servo5.write(0);
+  delay(1000); // Delay to allow servos to reach initial positions
+  servo3.write(120);
+    delay(1000); // D5elay to allow servos to reach initial positions
+  servo6.write(100);
+    delay(1000); // D5elay to allow servos to reach initial positions
+
+}
+
+}
